224e525c9a67cf02c4c6d6f977ed5b0631010085
Urban Faubion <urban.faubion@gmail.com>
2017-09-07 08:30:55 -0700

    Filtered out forked projects.

--- pages/projects/index.jsx
+++ pages/projects/index.jsx
@@ -3,30 +3,28 @@ import React from 'react'
 import DocumentTitle from 'react-document-title'
 import Repo from 'components/Repo'
 // $FlowFixMe
-import { config } from 'config'
+import {config} from 'config'
 import R from 'ramda'
-import { Future } from 'ramda-fantasy'
+import {Future} from 'ramda-fantasy'
 import styles from './_styles.module.css'
 
 export default class SiteIndex extends React.Component {
-  state = { repos: [] }
+  state = {repos: []}
 
-  componentDidMount () {
-    getRepos('https://api.github.com/users/urban/repos')
-      .fork(console.error,  (repos) => this.setState({ repos }))
+  componentDidMount() {
+    getRepos(
+      'https://api.github.com/users/urban/repos'
+    ).fork(console.error, repos => this.setState({repos}))
   }
 
-  render () {
-    const { repos } = this.state
+  render() {
+    const {repos} = this.state
     return (
       <DocumentTitle title={config.siteTitle}>
         <div>
           <h1>Projects</h1>
           <div className={styles.repos}>
-            {R.map(
-              ({ id, ...props }) => <Repo key={id} {...props} />,
-              repos
-            )}
+            {R.map(({id, ...props}) => <Repo key={id} {...props} />, repos)}
           </div>
         </div>
       </DocumentTitle>
@@ -34,26 +32,30 @@ export default class SiteIndex extends React.Component {
   }
 }
 
-const fetch = (url) => new Future((rej, res) => {
-  const req = new XMLHttpRequest()
-  req.addEventListener('load', function () {
-    this.status === 200
-      ? res(this.responseText)
-      : rej(this.responseText)
-  }, false)
-  req.addEventListener('error', rej, false)
-  req.addEventListener('abort', rej, false)
-  req.open('GET', url, true)
-  req.send()
-})
+const fetch = url =>
+  new Future((rej, res) => {
+    const req = new XMLHttpRequest()
+    req.addEventListener(
+      'load',
+      function() {
+        this.status === 200 ? res(this.responseText) : rej(this.responseText)
+      },
+      false
+    )
+    req.addEventListener('error', rej, false)
+    req.addEventListener('abort', rej, false)
+    req.open('GET', url, true)
+    req.send()
+  })
 
-const parseJSON = (str) => new Future((rej, res) => {
-  try {
-    res(JSON.parse(str))
-  } catch (err) {
-    rej({ error: err })
-  }
-})
+const parseJSON = str =>
+  new Future((rej, res) => {
+    try {
+      res(JSON.parse(str))
+    } catch (err) {
+      rej({error: err})
+    }
+  })
 
 const repoData = R.pick([
   'id',
@@ -62,15 +64,18 @@ const repoData = R.pick([
   'description',
   'language',
   'created_at',
-  'updated_at'
+  'updated_at',
 ])
 
 const getRepos = R.compose(
-  R.map(R.compose(
-    R.map(repoData),
-    R.reverse,
-    R.sortBy(R.prop('updated_at'))
-  )),
+  R.map(
+    R.compose(
+      R.map(repoData),
+      R.reverse,
+      R.sortBy(R.prop('updated_at')),
+      R.filter(R.propEq('fork', false))
+    )
+  ),
   R.chain(parseJSON),
   fetch
 )
