2dc1f786b4ee50812fbe86a02039dd6e27dc4a21
Urban Faubion <urban.faubion@gmail.com>
2024-09-06 06:31:52 -0700

    Feat: Remove old Gatsby source
    This commit removes the old GatsbJS website source that was
    contained in the 'archive' directory ad the workspace root.

--- archive/src/declarations.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "*.jpg";
-
-declare module "*/package.json" {
-  export const version: string;
-  export const author: string;
-}
-
-declare const graphql: (query: TemplateStringsArray) => void;
